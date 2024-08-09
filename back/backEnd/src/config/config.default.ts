fc3dbb41777f22a4526ad298080eec48000280c3
19271706221@163.com <19271706221@163.com>
2024-08-09 15:04:40 +0800

    Changes to be committed: 	modified:   back/backEnd/package-lock.json 	modified:   back/backEnd/package.json 	modified:   back/backEnd/src/config/config.default.ts

--- back/backEnd/src/config/config.default.ts
+++ back/backEnd/src/config/config.default.ts
@@ -1,9 +1,8 @@
-
 import { MidwayConfig } from '@midwayjs/core';
 
 export default {
   // use for cookie sign key, should change to your own and keep security
-  keys: '1722689238594_3013',
+  keys: '1723187005082_9970',
   koa: {
     port: 7001,
   },
